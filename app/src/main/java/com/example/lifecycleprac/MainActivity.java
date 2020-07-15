bc911ccaf0187e327e5fdc1c1a449d7044c6e946
arcolistro <arcolistro@gmail.com>
2020-07-15 04:11:04 -0700

    add intent to switch to second activity on click

--- app/src/main/java/com/example/lifecycleprac/MainActivity.java
+++ app/src/main/java/com/example/lifecycleprac/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.lifecycleprac;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button showGuess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showGuess = findViewById(R.id.button_guess);
+
+        showGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ShowGuess.class);
+                startActivity(intent);
+            }
+        });
 
         //show when onCreate() is triggered
         Toast.makeText(MainActivity.this, "onCreate() called",
